29683405f8dbf0eeb78fe4698a72eaa24b0ba0ea
anuragkr90 <anurag237@gmail.com>
2014-10-18 05:59:52 -0400

    Fifth Commit

--- src/main/java/edu/cmu/lti/f14/hw3/hw3_alnu/casconsumers/RetrievalEvaluator.java
+++ src/main/java/edu/cmu/lti/f14/hw3/hw3_alnu/casconsumers/RetrievalEvaluator.java
@@ -3,6 +3,8 @@ package edu.cmu.lti.f14.hw3.hw3_alnu.casconsumers;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +20,7 @@ import org.apache.uima.util.ProcessTrace;
 
 import edu.cmu.lti.f14.hw3.hw3_alnu.typesystems.Document;
 import edu.cmu.lti.f14.hw3.hw3_alnu.typesystems.Token;
+import edu.cmu.lti.f14.hw3.hw3_alnu.utils.Utils;
 
 
 public class RetrievalEvaluator extends CasConsumer_ImplBase {
@@ -30,7 +33,11 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 	public ArrayList<String> docList;
 	
-	public ArrayList<FSList> fslList;
+	public ArrayList<Map<String,Integer>> fslList;
+	
+	public ArrayList<Double> cosSim;
+	
+	public ArrayList<Integer> ListRank;
 	
 	public void initialize() throws ResourceInitializationException {
 
@@ -39,7 +46,10 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 		relList = new ArrayList<Integer>();
 		
 		docList = new ArrayList<String>();
-		fslList = new ArrayList<FSList>();
+		fslList = new ArrayList<Map<String,Integer>>();
+		cosSim = new ArrayList<Double>();
+		ListRank = new ArrayList<Integer>();
+		
 		
 	}
 
@@ -70,7 +80,11 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 			relList.add(doc.getRelevanceValue());
 			
 			docList.add(doc.getText());
-			fslList.add(doc.getTokenList());
+			
+			ArrayList<Token> querytk = Utils.fromFSListToCollection(doc.getTokenList(), Token.class);
+			Map<String, Integer> querymap = MakeMap(querytk);
+			//Map<String, Double> queryl1 = l1norm(querymap);
+			fslList.add(querymap);
 			//Do something useful here
 
 		}
@@ -81,7 +95,7 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	 * TODO 1. Compute Cosine Similarity and rank the retrieved sentences 2.
 	 * Compute the MRR metric
 	 */
-	private Map<String, Integer> CollectionToMap(Collection<Token> qToken){
+	private Map<String, Integer> MakeMap(Collection<Token> qToken){
 	  
 	  Map<String, Integer> queryret = new HashMap<String,Integer>();
 	  for(Token tk : qToken){
@@ -96,16 +110,63 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 		super.collectionProcessComplete(arg0);
 
+		int id = 0;		
+		for(id = 0 ; id < qIdList.size();){
+		  
+		  Map<String, Integer> qmap = fslList.get(id);
+		  cosSim.add(1.0);
+		  int nxt = id+1;
+		  
+		  for(nxt=id+1; nxt < qIdList.size();){
+		    
+		      if(qIdList.get(nxt)==qIdList.get(id)){
+		        Map<String, Integer> dmap = fslList.get(nxt);
+            double cosmeas = computeCosineSimilarity(qmap, dmap);
+		        //double cosmeas = computeEuclidean(qmap, dmap);
+            cosSim.add(cosmeas);
+            nxt++; 
+		      }
+		      else{
+		        break;
+		      }
+		    }
+		  
+		  id = nxt;
+		}
 		
-		// TODO :: compute the cosine similarity measure
+		id = 0;
+		for(id=0;id < qIdList.size();) {
+		ArrayList<Integer> newrklist = new ArrayList<Integer>();
+		int nxt = id + 1;
+		for(nxt=id+1;nxt < qIdList.size();){
+		  
+		  if(qIdList.get(nxt)==qIdList.get(id)){
+		    newrklist.add(nxt);
+		    nxt++;
+		  }
+		  else
+		    break;
+		}
+		Collections.sort(newrklist, new Comparator<Integer>() {
+		public int compare(Integer i1, Integer i2) {
+		return cosSim.get(i1).compareTo(cosSim.get(i2));
+		}
+		});
+		//Comparatorx<Integer> newcomp = new Comparatorx<Integer>();
+		//Collections.sort(newrklist,newcomp);
 		
-		
-		
-		// TODO :: compute the rank of retrieved sentences
-		
-		
-		
-		// TODO :: compute the metric:: mean reciprocal rank
+		Collections.reverse(newrklist);
+		for(int k = 0; k < newrklist.size(); k++) {
+		if(relList.get(newrklist.get(k)) == 1) {
+		ListRank.add(k + 1);
+		break;
+		}
+		}
+		id = nxt;
+		}
+		//System.out.println(ListRank);
+		//System.out.println(cosSim);
+		//System.out.println(cosSim.size());
 		double metric_mrr = compute_mrr();
 		System.out.println(" (MRR) Mean Reciprocal Rank ::" + metric_mrr);
 	}
@@ -114,6 +175,11 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	 * 
 	 * @return cosine_similarity
 	 */
+	//private class Comparatorx<Integer> {
+	//  public int compare(Integer i1, Integer i2){
+	//    return cosSim.get((java.lang.Integer) i1).compareTo(cosSim.get((java.lang.Integer) i2));
+	//  }
+	//}
 	private double computeCosineSimilarity(Map<String, Integer> queryVector,
 			Map<String, Integer> docVector) {
 		double cosine_similarity=0.0;
@@ -131,6 +197,9 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 		queryl2 = l2norm(queryl1);
 		docl2 = l2norm(docl1);
 		
+		//queryl2 = l2normint(queryVector);
+		//docl2 = l2normint(docVector);
+		
 		double currq = 0.0;
 		for(String st:queryl2.keySet()){
 		  
@@ -140,8 +209,47 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 		  }
 		  
 		}
+		cosine_similarity = Math.round((cosine_similarity * 10000))/10000.0;
 		return cosine_similarity;
 	}
+
+	
+	
+	
+	private double computeEuclidean(Map<String, Integer> queryVector,
+	        Map<String, Integer> docVector) {
+	      double cosine_similarity=0.0;
+
+	      // TODO :: compute cosine similarity between two sentences
+	      Map<String,Double> queryl1 = new HashMap<String,Double>();
+	      Map<String,Double> docl1 = new HashMap<String,Double>();
+	      
+	      Map<String,Double> docl2 = new HashMap<String,Double>();
+	      Map<String,Double> queryl2 = new HashMap<String,Double>();
+	      
+	      queryl1 = l1norm(queryVector);
+	      docl1 = l1norm(docVector);
+	      
+	      queryl2 = queryl1;
+	      docl2 = docl1;
+	      
+	      //queryl2 = l2normint(queryVector);
+	      //docl2 = l2normint(docVector);
+	      
+	      double currq = 0.0;
+	      for(String st:queryl2.keySet()){
+	        
+	        currq = queryl2.get(st);
+	        if(docl2.containsKey(st)){
+	          cosine_similarity = cosine_similarity + (currq-(docl2.get(st)))*(currq-(docl2.get(st)));
+	          //cosine_similarity = cosine_similarity + ((currq-(docl2.get(st)))*(currq-(docl2.get(st))))/(currq+(docl2.get(st)));
+	        }
+	        
+	      }
+	      cosine_similarity = Math.round((cosine_similarity * 10000))/10000.0;
+	      return cosine_similarity;
+	    }
+
 
 	private Map<String,Double> l1norm(Map<String, Integer> queryVector){
 	  
@@ -175,10 +283,29 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	    for (String tk : queryVector.keySet()){
 	      
 	      Double fqnorm = queryVector.get(tk);
-	      finalret.put(tk, Math.sqrt(fqnorm/fsum));
+	      finalret.put(tk, fqnorm/Math.sqrt(fsum));
 	    }
 	   return finalret; 
 	  }
+	 
+   private Map<String,Double> l2normint(Map<String, Integer> queryVector){
+     
+     Double fsum =0.0;
+     Map<String,Double> finalret = new HashMap<String,Double>();
+     
+     for(String tk : queryVector.keySet()){
+       
+       fsum = fsum + (queryVector.get(tk)*queryVector.get(tk)); 
+       
+     }
+     
+     for (String tk : queryVector.keySet()){
+       
+       int fqnorm = queryVector.get(tk);
+       finalret.put(tk, fqnorm/Math.sqrt(fsum));
+     }
+    return finalret; 
+   }
 	/**
 	 * 
 	 * @return mrr
@@ -187,8 +314,16 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 		double metric_mrr=0.0;
 
 		// TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
+		int ln = ListRank.size() ;
+		double invrank = 0.0;
+    for(int i = 0; i < ln ; i++) {
+    invrank += 1.0/ListRank.get(i);
+    }
+    metric_mrr = (1.0/ln)*invrank;
+    // TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
+    metric_mrr = Math.round(metric_mrr*10000)/10000.0;
+    return metric_mrr;
 		
-		return metric_mrr;
 	}
 
 }
